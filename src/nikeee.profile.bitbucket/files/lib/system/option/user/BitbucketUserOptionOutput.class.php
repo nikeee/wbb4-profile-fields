3ea2b8b634d6d25701d0a5efdf2fa58c1af366b4
nikeee13 <nikeee@outlook.com>
2013-11-10 20:36:33 +0100

    Konichiwa commit

---
+++ src/nikeee.profile.bitbucket/files/lib/system/option/user/BitbucketUserOptionOutput.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace wcf\system\option\user;
+use wcf\data\user\option\UserOption;
+use wcf\data\user\User;
+use wcf\util\StringUtil;
+
+/**
+ * User option output implementation for the output of a Bitbucket.org user profile.
+ * 
+ * @author	Niklas Mollenhauer
+ * @copyright	2013 Niklas Mollenhauer
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.option.user
+ * @category	Community Framework
+ */
+class BitbucketUserOptionOutput implements IUserOptionOutput {
+	/**
+	 * @see	wcf\system\option\user\IUserOptionOutput::getOutput()
+	 */
+	public function getOutput(User $user, UserOption $option, $value) {
+		if (empty($value)) return '';
+		
+		$value = trim($value);
+		$link = 'https://bitbucket.org/' . $value;
+
+		$link = StringUtil::encodeHTML($link);
+		$value = StringUtil::encodeHTML($value);
+
+		return '<a href="'.$link.'" class="externalURL"'.(EXTERNAL_LINK_REL_NOFOLLOW ? ' rel="nofollow"' : '').(EXTERNAL_LINK_TARGET_BLANK ? ' target="_blank"' : '').'>'.$value.'</a>';
+	}
+}
